6235bf3063b0f9458045fc86a4222854590b3835
La Min Tun <LaMin.Tun@academymortgage.com>
2024-05-07 14:14:28 -0500

    Disable format step in the workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,13 @@ module "network" {
   source                  = "./modules/network"
   resource_group_name     = azurerm_resource_group.resourcegroup.name
   resource_group_location = azurerm_resource_group.resourcegroup.location
-  vnet_name = "lamin-vnet-test"
-  subnet_name = "lamin-subnet-test"
+  vnet_name               = "lamin-vnet-test"
+  subnet_name             = "lamin-subnet-test"
 }
 
 module "apim" {
   source                  = "./modules/apim"
   resource_group_name     = azurerm_resource_group.resourcegroup.name
   resource_group_location = azurerm_resource_group.resourcegroup.location
-  subnet_id = module.network.subnet_id
+  subnet_id               = module.network.subnet_id
 }

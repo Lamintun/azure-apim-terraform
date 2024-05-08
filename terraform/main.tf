9e7a4652c9c448df84c06a333b6b72685ca2d12c
Lamintun <mr.lamintun@gmail.com>
2024-05-07 21:38:24 -0500

    Disable network integration in APIM

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,17 +4,17 @@ resource "azurerm_resource_group" "resourcegroup" {
   location = "eastus"
 }
 
-module "network" {
-  source                  = "./modules/network"
-  resource_group_name     = azurerm_resource_group.resourcegroup.name
-  resource_group_location = azurerm_resource_group.resourcegroup.location
-  vnet_name               = "lamin-vnet-test"
-  subnet_name             = "lamin-subnet-test"
-}
+# module "network" {
+#   source                  = "./modules/network"
+#   resource_group_name     = azurerm_resource_group.resourcegroup.name
+#   resource_group_location = azurerm_resource_group.resourcegroup.location
+#   vnet_name               = "lamin-vnet-test"
+#   subnet_name             = "lamin-subnet-test"
+# }
 
 module "apim" {
   source                  = "./modules/apim"
   resource_group_name     = azurerm_resource_group.resourcegroup.name
   resource_group_location = azurerm_resource_group.resourcegroup.location
-  subnet_id               = module.network.subnet_id
+  # subnet_id               = module.network.subnet_id
 }
